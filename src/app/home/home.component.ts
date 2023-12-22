785edd7ff15653f0a6aba8383d69e91d9ec007ff
Abraham Salazar <abraham_jack10@hotmail.com>
2023-12-22 12:58:51 -0300

    Add header section on home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  selected = new FormControl('');
 }
