812e9803c0e0f5dbd98c88a7ba13a2ca430868af
Abraham Salazar <abraham_jack10@hotmail.com>
2024-01-02 23:55:27 -0500

    Update routing for Home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent,
+    // loadComponent: () =>
+    //   import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+];
