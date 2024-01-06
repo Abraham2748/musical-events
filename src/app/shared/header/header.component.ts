27ea7a5e07f62a08306bd8acd39baaf71cf47f7b
Abraham Salazar <abraham_jack10@hotmail.com>
2024-01-05 22:58:49 -0500

    Styles updates on login, header and card components

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent, RouterLink],
+  imports: [SearchBarComponent, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  @Input({transform: booleanAttribute}) showLogoOnly: boolean = false;
 
 }
