5f54d35cd42225c7f71cff382960a031714d171b
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-20 21:18:25 -0500

    Implement search bar observable in service

--- src/app/home/home-header/home-header.component.ts
+++ src/app/home/home-header/home-header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { Observable } from 'rxjs';
+import { HomeHeaderService } from './home-header.service';
 
 @Component({
   selector: 'app-home-header',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.css',
 })
-export class HomeHeaderComponent {}
+export class HomeHeaderComponent {
+  searchBarFormControl = new FormControl();
+  homeHeaderService = inject(HomeHeaderService);
+
+  constructor() {
+    this.homeHeaderService.searchValue$ =
+      this.searchBarFormControl.valueChanges;
+  }
+}
