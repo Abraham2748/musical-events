3112ae7f7e92202fe8ae86038281774f5dc033d2
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-20 21:53:16 -0500

    Implementation of Footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeHeaderComponent } from './home-header/home-header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeaderComponent],
+  imports: [HomeHeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
