13e94ceed23955126132888c921a96880cf9ef1c
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-25 21:35:06 -0500

    Implement Event Card

--- src/app/shared/components/event-card/event-card.component.ts
+++ src/app/shared/components/event-card/event-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Concert } from '../../models/concert.model';
 @Component({
   selector: 'app-event-card',
   standalone: true,
   imports: [],
   templateUrl: './event-card.component.html',
-  styleUrl: './event-card.component.css'
+  styleUrl: './event-card.component.css',
 })
 export class EventCardComponent {
+  @Input({ required: true }) data!: Concert;
 
+  OnImageError() {
+    this.data.imageUrl = 'images/generic-concert-poster.jpg';
+  }
 }
