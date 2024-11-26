cec34073178825f91ae2588c3e1400705988ec93
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-25 22:01:10 -0500

    Upper section in Home Event List layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeaderComponent, FooterComponent],
+  imports: [
+    HomeHeaderComponent,
+    FooterComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
