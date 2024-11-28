102096907c9267aa5c4f44fe66ed14e89d2abd21
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-27 21:58:01 -0500

    Login layout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    SimpleHeaderComponent,
+    FooterComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
