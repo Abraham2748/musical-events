47f0bc1f3f3328db9940d0f91524a1af24f9c665
Abraham2748 <abraham_jack10@hotmail.com>
2024-12-02 21:36:16 -0500

    Register layout

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    SimpleHeaderComponent,
+    FooterComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    documentType: new FormControl('', [Validators.required]),
+    documentNumber: new FormControl('', [Validators.required]),
+  });
 }
