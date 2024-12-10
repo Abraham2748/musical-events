fe54015a580605cdf7dc0c9dbef5ccfaf03791a3
Abraham2748 <abraham_jack10@hotmail.com>
2024-12-09 21:57:01 -0500

    Update Home Header to handle user login

--- src/app/home/home-header/home-header.component.ts
+++ src/app/home/home-header/home-header.component.ts
@@ -4,16 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { Observable } from 'rxjs';
 import { HomeHeaderService } from './home-header.service';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
-    selector: 'app-home-header',
-    imports: [MatButtonModule, ReactiveFormsModule, RouterLink],
-    templateUrl: './home-header.component.html',
-    styleUrl: './home-header.component.css'
+  selector: 'app-home-header',
+  imports: [MatButtonModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './home-header.component.html',
+  styleUrl: './home-header.component.css',
 })
 export class HomeHeaderComponent {
   searchBarFormControl = new FormControl();
   homeHeaderService = inject(HomeHeaderService);
+  authService = inject(AuthService);
 
   constructor() {
     this.homeHeaderService.searchValue$ =
