f4631921e42c91e39827820e4df751128ea5dc43
Abraham2748 <abraham_jack10@hotmail.com>
2025-02-05 18:47:20 -0500

    clear main branch

--- src/app/home/home.model.ts
+++
@@ -1,8 +0,0 @@
-import { Concert } from '../shared/models/concert.model';
-import { Genre } from '../shared/models/genre.model';
-
-export interface HomeApiResponse {
-  concerts: Concert[];
-  genres: Genre[];
-  success: boolean;
-}
--- src/app/shared/pipes/text-limiter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextLimiterPipe } from './text-limiter.pipe';
-
-describe('TextLimiterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextLimiterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
