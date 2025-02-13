f92b2a58836f4fe9a8019055149fd95717841d93
Abraham2748 <abraham_jack10@hotmail.com>
2025-02-12 19:52:51 -0500

    Implement Simple Header

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
