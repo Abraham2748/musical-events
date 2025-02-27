970d39204b9606aff9933d5e32d69460bbc4ee2f
Abraham2748 <abraham_jack10@hotmail.com>
2025-02-26 21:34:17 -0500

    Implementación de la navegación en Customer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
+import { CustomerComponent } from './customer/customer.component';
+import { MyPurchasesComponent } from './customer/my-purchases/my-purchases.component';
+import { ChangePasswordComponent } from './customer/change-password/change-password.component';
 
 export const routes: Routes = [
   {
@@ -31,5 +34,25 @@ export const routes: Routes = [
     path: 'event-detail/:id',
     pathMatch: 'full',
     component: EventDetailComponent,
+  },
+  {
+    path: 'customer',
+    pathMatch: 'prefix',
+    component: CustomerComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'my-purchases',
+      },
+      {
+        path: 'my-purchases',
+        component: MyPurchasesComponent,
+      },
+      {
+        path: 'change-password',
+        component: ChangePasswordComponent,
+      },
+    ],
   },
 ];
