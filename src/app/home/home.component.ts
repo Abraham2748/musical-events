b0ea6b0e70e0cbe29ac66da71c89a6f512a2d2be
Abraham2748 <abraham_jack10@hotmail.com>
2025-04-28 20:20:37 -0500

    Implement Event List for Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { EventCardComponent } from '../shared/components/event-card/event-card.component';
 import { Concert } from '../shared/models/concert.model';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Genre } from '../shared/models/genre.model';
+import { HomeService } from '../shared/services/home.service';
+import { HighlightableDirective } from '../shared/directives/highlightable.directive';
 
 @Component({
   selector: 'app-home',
@@ -14,28 +18,59 @@ import { MatSelectModule } from '@angular/material/select';
     EventCardComponent,
     MatFormFieldModule,
     MatSelectModule,
+    ReactiveFormsModule,
+    HighlightableDirective,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  concertTest: Concert = {
-    id: 5,
-    title: ' Héctor Lavoe: El Cantante de los Cantantes',
-    description:
-      ' Un homenaje a la leyenda de la salsa, Héctor Lavoe, en una noche inolvidable.',
-    extendedDescription:
-      ' El legado de Héctor Lavoe, El Cantante de los Cantantes, se revive en un espectacular concierto homenaje que promete una velada inolvidable llena de ritmo, sabor y emoción. Con una orquesta en vivo que interpreta los grandes éxitos de Lavoe como “Periódico de Ayer”, “El Cantante” y “Mi Gente”, los asistentes se sumergirán en el alma y la pasión de la salsa pura. Esta noche estará dedicada a rendir tributo a una de las voces más icónicas de la música latina, cuyo impacto cultural sigue vigente en todo el mundo. El concierto estará acompañado de visuales que cuentan la historia de su carrera, desde su ascenso a la fama con la Fania All-Stars hasta su legado como el intérprete de salsa más influyente de todos los tiempos. La energía del público se sentirá en cada rincón, con fanáticos de todas las generaciones uniéndose para celebrar la vida y música de un verdadero ícono. Si eres amante de la salsa, este evento será una experiencia única, donde el espíritu de Héctor Lavoe vivirá a través de cada nota y cada canción.',
-    place: ' Gran Teatro Nacional',
-    unitPrice: 180.0,
-    genreId: 2,
-    genre: 'Salsa',
-    dateEvent: '12/01/2025',
-    timeEvent: '20:00',
-    imageUrl:
-      'http://localhost:8080/concerts/f21f119c-f02f-41e2-8d20-74dfecde7fc6.jpg',
-    ticketsQuantity: 110,
-    finalized: false,
-    status: 'Activo',
-  };
+export class HomeComponent implements OnInit {
+  concerts: Concert[] = [];
+  genres: Genre[] = [];
+  initialConcerts: Concert[] = [];
+  currentGenre = new FormControl(0);
+  searchGenreValue = 0;
+  searchBarValue = '';
+
+  homeService = inject(HomeService);
+
+  ngOnInit(): void {
+    this.homeService.getData().subscribe((res) => {
+      this.initialConcerts = res.concerts;
+      this.genres = res.genres;
+      this.concerts = this.initialConcerts;
+    });
+
+    this.currentGenre.valueChanges.subscribe((value: number | null) => {
+      this.searchGenreValue = value || 0;
+      this.filterConcerts();
+    });
+  }
+  filterConcerts() {
+    this.filterByGenre();
+    this.filterByDescription();
+  }
+  filterByGenre() {
+    if (this.searchGenreValue === 0) {
+      this.concerts = this.initialConcerts;
+    } else {
+      this.concerts = this.initialConcerts.filter(
+        (concert) => concert.genreId === this.searchGenreValue
+      );
+    }
+  }
+  filterByDescription() {
+    if (!this.searchBarValue) return;
+
+    this.concerts = this.concerts.filter((concert) =>
+      concert.description
+        .toLowerCase()
+        .includes(this.searchBarValue.toLowerCase())
+    );
+  }
+
+  onSearchBarValueChange(value: string) {
+    this.searchBarValue = value;
+    this.filterConcerts();
+  }
 }
