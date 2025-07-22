eb29e44ddc82642f9d7f8aa6f88d15522b9fc4f3
Abraham Salazar <abraham_jack10@hotmail.com>
2025-07-21 21:29:11 -0500

    Implement Login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SimpleHeader } from '../shared/components/simple-header/simple-header';
 import { Footer } from '../shared/components/footer/footer';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../shared/services/auth';
+import { catchError, of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -21,4 +29,36 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  authService = inject(Auth);
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+  router = inject(Router);
+
+  login() {
+    const email = this.loginForm.controls.email.value!;
+    const password = this.loginForm.controls.password.value!;
+
+    this.authService
+      .login(email, password)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          alert(err.error.errorMessage);
+          console.log('error: ', err);
+          return of();
+        })
+      )
+      .subscribe((res) => {
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('tokenExpiration', res.data.expirationDate);
+        this.authService.decodeToken();
+        alert('Bienvenido');
+        this.router.navigateByUrl('/');
+      });
+  }
+}
---
+++ src/app/shared/services/auth.ts
@@ -0,0 +1,72 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { LoginApiResponse } from '../models/auth.model';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private baseUrl = 'http://localhost:8080/api/';
+  private http = inject(HttpClient);
+
+  private tokenExpiration = signal(new Date());
+
+  private role = signal('');
+  private email = signal('');
+  private name = signal('');
+
+  private isLoggedIn = signal(false);
+
+  getEmail() {
+    return this.email();
+  }
+
+  getName() {
+    return this.name();
+  }
+
+  getRole() {
+    return this.role();
+  }
+
+  getTokenExpiration() {
+    return this.tokenExpiration();
+  }
+
+  getIsLoggedIn() {
+    return this.isLoggedIn();
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<LoginApiResponse>(this.baseUrl + 'users/login', {
+      username: email,
+      password,
+    });
+  }
+
+  decodeToken() {
+    const token = localStorage.getItem('token');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    if (!token || !tokenExpiration) return;
+
+    this.tokenExpiration.set(new Date(tokenExpiration));
+
+    const jwtDecoded = jwtDecode<any>(token);
+
+    this.role.set(
+      jwtDecoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+    );
+    this.email.set(
+      jwtDecoded[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
+      ]
+    );
+    this.name.set(
+      jwtDecoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name']
+    );
+
+    this.isLoggedIn.set(true);
+  }
+}
