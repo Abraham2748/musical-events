a173387014b6d75061d3f44b7107d09fc597fad0
Abraham Salazar <abraham_jack10@hotmail.com>
2025-07-21 19:54:24 -0500

    Implement Login layout

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 import { SimpleHeader } from '../shared/components/simple-header/simple-header';
+import { Footer } from '../shared/components/footer/footer';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [SimpleHeader],
+  imports: [
+    SimpleHeader,
+    Footer,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
