c4ecfb5a8dc27362c12c12caf38089515482693b
Abraham Salazar <abraham_jack10@hotmail.com>
2025-08-04 20:06:05 -0500

    Implement Customer Menu

--- src/app/customer/customer.ts
+++ src/app/customer/customer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LoggedInHeader } from '../shared/components/logged-in-header/logged-in-header';
+import { Footer } from '../shared/components/footer/footer';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [LoggedInHeader, Footer, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './customer.html',
-  styleUrl: './customer.css'
+  styleUrl: './customer.css',
 })
-export class Customer {
-
-}
+export class Customer {}
