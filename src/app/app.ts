89b575aaebba4863c388422c89db6d3ac8e14e8c
Abraham Salazar <abraham_jack10@hotmail.com>
2025-10-15 21:55:19 -0500

    Example of creation of components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HolaMundo } from './hola-mundo/hola-mundo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, HolaMundo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('musical-events');
