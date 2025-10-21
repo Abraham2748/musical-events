8eccb3af715dc87a6296de2f29d46cb519413227
Abraham Salazar <abraham_jack10@hotmail.com>
2025-10-20 21:39:13 -0500

    Implement footer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeHeader } from './home-header/home-header';
+import { Footer } from '../shared/components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHeader],
+  imports: [HomeHeader, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
