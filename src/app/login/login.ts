5aeec6fd89752f963d7184f37ad05c41ab97f4ed
Abraham2748 <abraham_jack10@hotmail.com>
2025-10-27 20:13:39 -0500

    Create login layout

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SimpleHeader } from '../shared/components/simple-header/simple-header';
+import { Footer } from '../shared/components/footer/footer';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [SimpleHeader, Footer, MatFormFieldModule, MatInputModule, RouterLink, MatButtonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
-export class Login {
-
-}
+export class Login {}
